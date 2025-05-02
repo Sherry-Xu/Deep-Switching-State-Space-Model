02f587d6ba6af01d16cadef43c4959a875f29d04
SebastianPhq <128215994+SebastianPhq@users.noreply.github.com>
2025-05-02 12:19:52 +0800

    update

--- retrain_experiment.sh
+++ retrain_experiment.sh
@@ -1,18 +1,17 @@
-python main.py -p Toy --train
-python main.py -p Toy --train
-python main.py -p Toy --train
-python main.py -p Toy --train
-python main.py -p Toy --train
-python main.py -p Toy --train
+python main.py -p Toy --train --seed 12
+python main.py -p Toy --train --seed 5
+python main.py -p Toy --train --seed 4
+python main.py -p Toy --train --seed 0
+python main.py -p Toy --train --seed 2
+python main.py -p Lorenz --train --seed 16
+python main.py -p Lorenz --train --seed 5
+python main.py -p Lorenz --train --seed 2
+python main.py -p Lorenz --train --seed 4
 python main.py -p Lorenz --train
-python main.py -p Lorenz --train 
-python main.py -p Lorenz --train 
-python main.py -p Lorenz --train 
-python main.py -p Lorenz --train 
-python main.py -p Sleep --train 
+python main.py -p Sleep --train
 python main.py -p Unemployment --train
-python main.py -p Hangzhou --train 
+python main.py -p Hangzhou --train
 python main.py -p Seattle --train
-python main.py -p Pacific --train
+python main.py -p Pacific --train 
 python main.py -p Electricity --train
 python table_generation.py --train
